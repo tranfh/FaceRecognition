e375e5608a2c478beb195266e3e61a87098403bc
Frank Tran <frank.tran@live.ca>
2021-09-02 11:09:24 -0400

    Add FaceRecognition CSS and Functionality

--- src/App.js
+++ src/App.js
@@ -18,8 +18,28 @@ class App extends Component {
     this.state = {
       input: '',
       imageURL: '',
+      box: {},
     };
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    };
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+    console.log(this.state.box);
+  };
 
   onInputChange = (event) => {
     console.log(event.target.value);
@@ -29,16 +49,12 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageURL: this.state.input });
     console.log(this.state.imageURL);
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function (response) {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      },
-      function (err) {
-        console.log('Error, ', err);
-      }
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch((err) => console.log('Error, ', err));
   };
 
   render() {
@@ -51,7 +67,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageURL={this.state.imageURL} />
+        <FaceRecognition imageURL={this.state.imageURL} box={this.state.box} />
         <ParticlesBg className="particles" type="lines" bg={true} />
       </div>
     );
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageURL }) => {
+const FaceRecognition = ({ imageURL, box }) => {
   return (
     <div className="ma center">
-      <div className="absoolute mt2">
-        <img src={imageURL} alt="" width="500px" height="auto"></img>
+      <div className="absolute mt2">
+        <img id="inputImage" src={imageURL} alt="" width="500px" height="auto">
+          </img>
+          <div
+            className="bounding-box"
+            style={{
+              top: box.topRow,
+              right: box.rightCol,
+              left: box.leftCol,
+              bottom: box.bottomRow,
+            }}
+          ></div>
       </div>
     </div>
   );
